048b8e98b4266486416fddbe0289d364ddc0d68e
TimurYamchuk <134388477+TimurYamchuk@users.noreply.github.com>
2023-10-13 14:26:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,174 +1,58 @@
 #include <iostream>
+#include <map>
 #include <string>
+
 using namespace std;
 
-class Base 
-{
-public:
-    static int people_on_base;
-    static int vehicles_on_base;
-    static double petrol_on_base;
-    static double goods_on_base;
-};
+map<string, string> dictionary;
 
-int Base::people_on_base = 0;
-int Base::vehicles_on_base = 0;
-double Base::petrol_on_base = 10000.0; 
-double Base::goods_on_base = 500.0;   
+int main() {
+    int choice;
+    string word, definition;
 
-class Vehicle 
-{
-protected:
-    double petrol_amount;
-    double tank_volume;
+    while (true) {
+        cout << "\nМеню:" << endl;
+        cout << "1. Добавить/Обновить слово" << endl;
+        cout << "2. Удалить слово" << endl;
+        cout << "3. Поиск слова" << endl;
+        cout << "4. Выйти" << endl;
+        cout << "Выберите действие: ";
+        cin >> choice;
 
-public:
-    Vehicle(double petrol_amount, double tank_volume)
-        : petrol_amount(petrol_amount), tank_volume(tank_volume) {}
-    
-    double getPetrolAmount() 
-    {
-        return petrol_amount;
-    }
+        switch (choice) {
+            case 1:
+                cout << "Слово: ";
+                cin >> word;
+                cout << "Определение: ";
+                cin.ignore();
+                getline(cin, definition);
+                dictionary[word] = definition;
+                break;
 
-    double getTankVolume() 
-    {
-        return tank_volume;
-    }
+            case 2:
+                cout << "Слово для удаления: ";
+                cin >> word;
+                dictionary.erase(word);
+                break;
 
-    virtual void arrive()
-    {
-        Base::people_on_base++;
-        Base::vehicles_on_base++;
-    }
+            case 3:
+                cout << "Слово для поиска: ";
+                cin >> word;
+                if (dictionary.find(word) != dictionary.end()) {
+                    cout << "Слово: " << word << ", Значение: " << dictionary[word] << endl;
+                } else {
+                    cout << "Слово '" << word << "' не найдено в словаре." << endl;
+                }
+                break;
 
-    virtual bool leave() 
-    {
-        if (petrol_amount < tank_volume && Base::people_on_base == 0) 
-        {
-            return false; 
+            case 4:
+                cout << "Программа завершена." << endl;
+                return 0;
+
+            default:
+                cout << "Неверный выбор. Пожалуйста, выберите действие снова." << endl;
+                break;
         }
-
-        petrol_amount = tank_volume;
-     
-        Base::people_on_base--;
-        Base::vehicles_on_base--;
-
-        return true;
-    }
-};
-
-class Bus : public Vehicle 
-{
-    int people;
-    int max_people;
-
-public:
-    Bus(int people, int max_people, double petrol, double max_petrol)
-        : Vehicle(petrol, max_petrol), people(people), max_people(max_people) {}
-
-    int getPeopleCount() 
-    {
-        return people;
-    }
-
-    int getMaxPeople() 
-    {
-        return max_people;
-    }
-
-    void arrive() override 
-    {
-        Base::people_on_base += people;
-        Base::vehicles_on_base++;
-    }
-
-    bool leave() override 
-    {
-        if (!Vehicle::leave()) 
-        {
-            return false; 
-        }
-
-        Base::people_on_base -= people;
-
-        return true;
-    }
-};
-
-class Truck : public Vehicle 
-{
-    double load;
-    double max_load;
-
-public:
-    Truck(double load, double max_load, double petrol, double max_petrol)
-        : Vehicle(petrol, max_petrol), load(load), max_load(max_load) {}
-
-    double getCurrentLoad()
-    {
-        return load;
-    }
-
-    double getMaxLoad()
-    {
-        return max_load;
-    }
-
-    void arrive() override
-    {
-        Base::vehicles_on_base++;
-        Base::goods_on_base += load;
-    }
-
-    bool leave() override 
-    {
-        if (!Vehicle::leave()) 
-        {
-            return false; 
-        }
-
-        Base::goods_on_base -= load;
-
-        return true;
-    }
-};
-
-int main() 
-{
-    Vehicle car(30.0, 50.0);
-    Bus bus(20, 50, 20.0, 100.0);
-    Truck truck(10.0, 20.0, 15.0, 80.0);
-
-    car.arrive();
-    bus.arrive();
-    truck.arrive();
-
-    if (car.leave()) 
-    {
-        cout << "Car left the base" << endl;
-    }
-    else
-    {
-        cout << "Car couldn't leave the base" << endl;
-    }
-
-    if (bus.leave()) 
-    {
-        cout << "Bus left the base" << endl;
-    }
-    else 
-    {
-        cout << "Bus couldn't leave the base" << endl;
-    }
-
-    if (truck.leave()) 
-    {
-        cout << "Truck left the base" << endl;
-    }
-    else 
-    {
-        cout << "Truck couldn't leave the base" << endl;
     }
 
     return 0;
